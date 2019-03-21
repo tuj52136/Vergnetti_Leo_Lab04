1c1f74512dc39ee91cbc8cd4dccee869714c9c9d
Leo Vergnetti <tuj52136@cst11513.tu.temple.edu>
2019-03-21 11:17:09 -0400

    Initial Commit

---
+++ Vergnetti_Leo_Lab04/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Vergnetti_Leo_Lab04
+//
+//  Created by Leo Vergnetti on 3/21/19.
+//  Copyright © 2019 Leo Vergnetti. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
